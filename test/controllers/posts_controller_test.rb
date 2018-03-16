058099db41ebc819a73d31f16b8c2417a1bc4814
Bob Forcha <bobforcha@gmail.com>
2018-03-16 13:51:51 -0400

    create controller tests. BROKEN.

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "redirects anonymous users to login" do
+    get :index
+    assert_redirected_to  login_url
+  end
+
+  test "get index for authorized users" do
+    user1 = users(:user1)
+
+    get :index, {}, { user_id: user1.id}
+    assert_response :sucess
+  end
 end
