4e1db3a1d4e0411085487be2c8beb51d8e4b6e6f
Bob Forcha <bobforcha@gmail.com>
2018-03-29 13:22:19 -0400

    implemented low-level caching for comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,10 @@ class Post < ApplicationRecord
   validates :type, presence: true
 
   has_many :comments, dependent: :destroy
+
+  def cached_comment_count
+    Rails.cache.fetch [self, "comment_count"] do
+      comments.size
+    end
+  end
 end
